c940a02d23e4233ad9ce2a27f5295582d9549a43
Dickens <dickenslian@gmail.com>
2016-02-19 15:21:36 +0800

    complete part 4

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var path = require('path');
+var config = require('../webpack.config.js');
+var webpack = require('webpack');
+var webpackDevMiddleare = require('webpack-dev-middleware');
+var webpackHotMiddleare = require('webpack-hot-middleware');
+
 
 var app = express();
+
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleare(compiler, {noInfo:true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleare(compiler));
 
 app.use(express.static('./dist'));
 
